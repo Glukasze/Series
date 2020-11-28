f0b6a9f91a220022a60babfdde2fe99086ab4230
Rozan <gracjan.luk@gmail.com>
2020-11-28 15:52:16 +0100

    implemented pagination

--- main.py
+++ main.py
@@ -16,15 +16,21 @@ def index():
 def design():
     return render_template('design.html')
 
+@app.route('/shows/<int:page>')
 @app.route('/shows/most-rated')
-def most_rated():
+def most_rated(page=False):
     ROWS_PER_PAGE = 15
+    INITIAL_ROWS = 15
     offset = 0
-    number_of_pages = int(queries.get_number_of_rows()[0]['count'] / ROWS_PER_PAGE)
-    shows_15_best = queries.get_15_best(offset)
-    return render_template('most_rated.html', shows_15_best=shows_15_best, number_of_pages=number_of_pages)
+    number_of_pages = int((queries.get_number_of_rows()[0]['count'] - INITIAL_ROWS) / ROWS_PER_PAGE)
+    if page != False:
+        offset = ROWS_PER_PAGE * page - INITIAL_ROWS
 
-@app.route('/shows/<id>')
+    shows_15_best = queries.get_15_best(offset)
+
+    return render_template('most_rated.html', shows_15_best=shows_15_best, number_of_pages=number_of_pages, page=page)
+
+@app.route('/show/<id>')
 def shows_id(id):
 
     return render_template('shows_id.html', id=id)
