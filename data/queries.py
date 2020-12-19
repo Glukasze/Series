a8bb3c542dcdee638a9532360b4d8901efa42552
Rozan <gracjan.luk@gmail.com>
2020-12-19 14:28:55 +0100

    final commit

--- data/queries.py
+++ data/queries.py
@@ -35,7 +35,7 @@ def get_sorted(order, direction, limit, offset):
                                                         limit = sql.Literal(limit),
                                                         offset = sql.Literal(offset)), {'direction':direction})
 
-def get_detailed(id):
+def get_detailed(show_id):
     return data_manager.execute_select(sql.SQL("""
     SELECT shows.title, shows.runtime, ROUND(shows.rating, 1),
     STRING_AGG(genres.name, ', ' ORDER BY name) genres, shows.overview, shows.trailer
@@ -44,18 +44,18 @@ def get_detailed(id):
     ON shows.id = show_genres.show_id
     LEFT JOIN genres
     ON genres.id = show_genres.genre_id
-    WHERE shows.id = {id}
-    GROUP BY shows.title, shows.runtime, shows.rating, shows.overview, shows.trailer;""").format(id = sql.Literal(id)))
+    WHERE shows.id = {show_id}
+    GROUP BY shows.title, shows.runtime, shows.rating, shows.overview, shows.trailer;""").format(show_id = sql.Literal(show_id)))
 
-def get_actors(id):
+def get_actors_by_show_id(show_id):
     return data_manager.execute_select(sql.SQL("""
     SELECT actors.name FROM actors
     LEFT JOIN show_characters
     ON actors.id = show_characters.actor_id
     LEFT JOIN shows
     ON show_characters.show_id = shows.id
-    WHERE shows.id = {id}
-    LIMIT 3;""").format(id = sql.Literal(id)))
+    WHERE shows.id = {show_id}
+    LIMIT 3;""").format(show_id = sql.Literal(show_id)))
 
 def get_seasons(id):
     return data_manager.execute_select(sql.SQL("""
@@ -64,3 +64,56 @@ def get_seasons(id):
     LEFT JOIN shows
     ON seasons.show_id = shows.id
     WHERE shows.id = {id};""").format(id = sql.Literal(id)))
+
+def get_names_by_rating(rating):
+    return data_manager.execute_select(sql.SQL("""
+    SELECT title
+    FROM shows
+    WHERE ROUND(rating, 1) = {rating}""").format(rating = sql.Literal(rating)))
+
+def get_characters_by_actor(actor):
+    return data_manager.execute_select(sql.SQL("""
+    SELECT actors.name, show_characters.character_name, shows.title, TO_CHAR(shows.year, 'YYYY')
+    FROM actors
+    LEFT JOIN show_characters
+    ON actors.id = show_characters.actor_id
+    LEFT JOIN shows
+    ON show_characters.show_id = shows.id
+    WHERE LOWER(actors.name) LIKE LOWER({actor})
+    ORDER BY actors.name""").format(actor = sql.Literal(actor)))
+
+def get_genres_and_actors():
+    return data_manager.execute_select(sql.SQL("""
+    SELECT actors.id, actors.name, STRING_AGG(DISTINCT genres.name, ', ' ORDER BY genres.name) genres
+    FROM actors
+    LEFT JOIN show_characters ON actors.id = show_characters.actor_id
+    LEFT JOIN shows ON show_characters.show_id = shows.id
+    LEFT JOIN show_genres ON shows.id = show_genres.show_id
+    LEFT JOIN genres ON show_genres.genre_id = genres.id
+    GROUP BY actors.id"""))
+
+def get_genres_by_actors(actor):
+    return data_manager.execute_select(sql.SQL("""
+    SELECT actors.id, actors.name, STRING_AGG(DISTINCT genres.name, ', ' ORDER BY genres.name) genres
+    FROM actors
+    LEFT JOIN show_characters ON actors.id = show_characters.actor_id
+    LEFT JOIN shows ON show_characters.show_id = shows.id
+    LEFT JOIN show_genres ON shows.id = show_genres.show_id
+    LEFT JOIN genres ON show_genres.genre_id = genres.id
+    WHERE LOWER(actors.name) LIKE LOWER({actor})
+    GROUP BY actors.id""").format(actor=sql.Literal(actor)))
+
+def get_actors_by_show(show):
+    return data_manager.execute_select(sql.SQL("""
+    SELECT actors.name, show_characters.character_name, shows.title
+    FROM actors
+    LEFT JOIN show_characters ON actors.id = show_characters.actor_id
+    LEFT JOIN shows ON show_characters.show_id = shows.id
+    WHERE LOWER(shows.title) LIKE LOWER({show})
+    GROUP BY actors.name, show_characters.character_name, shows.title""").format(show=sql.Literal(show)))
+
+def get_game():
+    return data_manager.execute_select(sql.SQL("""
+    SELECT episodes.episode_number, episodes.title
+    FROM episodes
+    WHERE episodes.id = 73630"""))
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect, jsonify
 from data import queries
-import math
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,6 +8,7 @@ app = Flask('codecool_series')
 ROWS_PER_PAGE = 15
 PAGES_SHOWN = 5
 
+# setting correct order for sorting to avoid logical expressions in HTML
 def get_order(page, old_order, order, direction):
     string = f'/shows/{page}/{order}/'
 
@@ -19,18 +19,47 @@ def get_order(page, old_order, order, direction):
     else:
         return string + direction
 
-def print_arrow():
-    return '&darr'
-
+# welcome page
 @app.route('/')
 def index():
     shows = queries.get_shows()
     return render_template('index.html', shows=shows)
 
-
+# dummy page with the default layout
 @app.route('/design')
 def design():
     return render_template('design.html')
+
+# def find_movies(shows_all, selected_rating):
+#     result = []
+#     for item in shows_all:
+#         if str(item['round']) == selected_rating:
+#             result.append(item)
+#     return result
+
+#JSON routes
+
+@app.route('/by_actor/<show>')
+def by_actor(show):
+
+    show = "%" + show + "%"
+    selected = queries.get_actors_by_show(show)
+    return jsonify(selected)
+
+@app.route('/by_rating/<rating>')
+def by_rating(rating):
+    selected = queries.get_names_by_rating(rating)
+    return jsonify(selected)
+
+@app.route('/data/testing/<show>')
+def data_testing(show):
+    show = '%' + show + '%'
+    selected = queries.get_actors_by_show(show)
+    return jsonify(selected)
+
+
+
+# main page with sorting
 
 # @app.route('/shows')
 # @app.route('/shows/<order>')
@@ -38,16 +67,19 @@ def design():
 # @app.route('/shows/<int:page>')
 # @app.route('/shows/<int:page>/<order>')
 @app.route('/shows/<int:page>/<order>/<direction>')
-@app.route('/shows/most-rated')
+@app.route('/shows/most-rated', methods=['POST', 'GET'])
 # @app.route('/shows/most-rated/<order>')
 # @app.route('/shows/most-rated/<order>/<direction>')
 def most_rated(page=1, order='rating', direction='DESC'):
 
+
     number_of_pages = int((queries.get_number_of_shows()['count']) / ROWS_PER_PAGE) + 1
     offset = ROWS_PER_PAGE * page - ROWS_PER_PAGE
 
+    # ready sql data
     shows_sorted = queries.get_sorted(order, direction, ROWS_PER_PAGE, offset)
 
+    # pagination
     pages_shown_start = int(page - ((PAGES_SHOWN - 1) / 2))
     pages_shown_end = int(page + ((PAGES_SHOWN - 1) / 2))
 
@@ -66,14 +98,17 @@ def most_rated(page=1, order='rating', direction='DESC'):
                            number_of_pages=number_of_pages,
                            direction=direction,
                            order=order,
-                           get_order=get_order,
-                           print_arrow=print_arrow)
+                           get_order=get_order)
 
-@app.route('/show/<int:id>')
-def shows_id(id):
+# page with detailed view on a given show
+@app.route('/show/<int:show_id>')
+def shows_id(show_id):
 
-# TODO get ready int from queries
-    show_detailed = queries.get_detailed(id)
+    # ready sql data
+    show_detailed = queries.get_detailed(show_id)
+    print(show_detailed)
+
+    # formatting runtime to show hours and minutes if >60 and only minutes if <60
     runtime_formatted = divmod(show_detailed[0]['runtime'], 60)
     if runtime_formatted[0] < 1:
         runtime_formatted = f"{runtime_formatted[1]}min"
@@ -82,7 +117,8 @@ def shows_id(id):
     else:
         runtime_formatted = f"{runtime_formatted[0]}h {runtime_formatted[1]}min"
 
-    actors_raw = queries.get_actors(id)
+    # preparing 3 top actors
+    actors_raw = queries.get_actors_by_show_id(show_id)
     actors = []
     for actor in actors_raw:
         if len(actors) < 2:
@@ -90,7 +126,7 @@ def shows_id(id):
         else:
             actors.append(actor['name'])
 
-    seasons = queries.get_seasons(id)
+    seasons = queries.get_seasons(show_id)
     for season in seasons:
         print(season)
 
@@ -99,6 +135,15 @@ def shows_id(id):
                            runtime_formatted=runtime_formatted,
                            actors=actors,
                            seasons=seasons)
+
+# page for testing
+@app.route('/testing')
+def testing():
+
+    episode = queries.get_game()
+
+    return render_template('testing.html', episode=episode)
+
 
 def main():
     app.run(debug=False)
